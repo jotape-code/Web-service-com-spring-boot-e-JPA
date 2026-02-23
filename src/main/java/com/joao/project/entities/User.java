6f65db61b079ef4bc47b59b95d7d879e103f7bc5
João pedro Floriano <joaopflori23@gmail.com>
2026-02-23 11:02:24 -0300

    Fix user hashcode and equals

--- src/main/java/com/joao/project/entities/User.java
+++ src/main/java/com/joao/project/entities/User.java
@@ -53,16 +53,32 @@ public class User implements Serializable{
         this.password = password;
     }
     @Override
-    public boolean equals(Object o){
-        if(this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id != null && id.equals(user.id);
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
     
     @Override
-    public int hashCode(){
-        return id != null ? id.hashCode() : 0;
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
-    
 }
