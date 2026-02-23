2585dbe0adf14f2abe9d3d4a209db26af8657dfd
João pedro Floriano <joaopflori23@gmail.com>
2026-02-23 10:59:14 -0300

    First commit

---
+++ src/main/java/com/joao/project/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.joao.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joao.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "joão", "joao@gmail.com", "421321", "412421");
+        return ResponseEntity.ok().body(u);
+    }
+}
