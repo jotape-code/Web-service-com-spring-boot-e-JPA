f9e5f7c3a093094387e53a29e2dc9a15e6de9b50
João pedro Floriano <joaopflori23@gmail.com>
2026-02-23 11:33:11 -0300

    H2 database, test profile, JPA

--- src/main/java/com/joao/project/entities/User.java
+++ src/main/java/com/joao/project/entities/User.java
@@ -2,10 +2,20 @@ package com.joao.project.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
     
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -51,10 +61,6 @@ public class User implements Serializable{
     }
     public void setPassword(String password){
         this.password = password;
-    }
-    @Override
-    public String toString() {
-        return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + "]";
     }
 
     @Override
